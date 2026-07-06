fca9cf939717396c959c283cb0424be312397fed
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-07-07 06:26:14 +0800

    Add app routes

--- lib/app/router/app_routes.dart
+++ lib/app/router/app_routes.dart
@@ -1,3 +1,8 @@
 abstract final class AppRoutes {
+  static const splash = '/splash';
   static const home = '/';
+  static const newNote = '/new-note';
+  static const review = '/review';
+  static const identity = '/identity';
+  static const settings = '/settings';
 }
