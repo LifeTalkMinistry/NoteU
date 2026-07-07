aaf42d72f970cf50910e6e997dea16f19e1c632d
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-07-07 23:23:04 +0800

    Register My Notes screen route

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 
 import '../../features/home/presentation/screens/home_screen.dart';
 import '../../features/identity/presentation/screens/learning_identity_screen.dart';
+import '../../features/notes/presentation/screens/my_notes_screen.dart';
 import '../../features/notes/presentation/screens/new_note_screen.dart';
 import '../../features/review/presentation/screens/review_screen.dart';
 import '../../features/settings/presentation/screens/settings_screen.dart';
@@ -16,6 +17,7 @@ final appRouterProvider = Provider<GoRouter>((ref) {
       GoRoute(path: AppRoutes.splash, builder: (context, state) => const LaunchScreen()),
       GoRoute(path: AppRoutes.home, builder: (context, state) => const HomeScreen()),
       GoRoute(path: AppRoutes.newNote, builder: (context, state) => const NewNoteScreen()),
+      GoRoute(path: AppRoutes.myNotes, builder: (context, state) => const MyNotesScreen()),
       GoRoute(path: AppRoutes.review, builder: (context, state) => const ReviewScreen()),
       GoRoute(path: AppRoutes.identity, builder: (context, state) => const LearningIdentityScreen()),
       GoRoute(path: AppRoutes.settings, builder: (context, state) => const SettingsScreen()),
